0f0d3645bf4d422cc9b9fda9e9c0cdc94a5d60a3
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-02-27 11:00:23 -0300

    User services UPDATE v1

--- controllers/services.controller.js
+++ controllers/services.controller.js
@@ -1,6 +1,6 @@
 /* import User from '../models/user.js'; */
 
-const User = require('../models/user.js');
+const User = require("../models/user.js");
 
 /* servicios: [{
         nombre: String,
@@ -12,80 +12,115 @@ const User = require('../models/user.js');
         fecha: Date
 }],*/
 
-const getServices= async (req, res) => {
-    const {user_id} = req.user;
-    const response = await User.findById(user_id)
-    const services = response.servicios
-    response ? res.status(200).json(services) : res.status(404).json({message: "No es un id Valido"});
+const getServices = async (req, res) => {
+  const { user_id } = req.user;
+  const response = await User.findById(user_id);
+  const services = response.servicios;
+  response
+    ? res.status(200).json(services)
+    : res.status(404).json({ message: "No es un id Valido" });
 };
 
-const createService= async (req, res) => {
-    const {user_id} = req.user;
-    const {nombre, precio, cantidadVendidos, cantidadDisponibles, descripcion, imagen} = req.body;
-    const response = await User.findById(user_id)
-    const services = response.servicios
-    const newService = {
-        nombre,
-        precio,
-        cantidadVendidos,
-        cantidadDisponibles,
-        descripcion
-    }
-    services.push(newService)
-    response.servicios = services
-    const result = await response.save()
-    result ? res.status(200).json(result) : res.status(404).json({message: "No es un id Valido"});
+const createService = async (req, res) => {
+  const { user_id } = req.user;
+  const {
+    nombre,
+    precio,
+    cantidadVendidos,
+    cantidadDisponibles,
+    descripcion,
+    imagen,
+  } = req.body;
+  const response = await User.findById(user_id);
+  const services = response.servicios;
+  const newService = {
+    nombre,
+    precio,
+    cantidadVendidos,
+    cantidadDisponibles,
+    descripcion,
+  };
+  services.push(newService);
+  response.servicios = services;
+  const result = await response.save();
+  result
+    ? res.status(200).json(result)
+    : res.status(404).json({ message: "No es un id Valido" });
 };
 
 const itemService = async (req, res) => {
-    const {user_id} = req.user;
-    const response = await User.findById(user_id)
-    const services = response.servicios
-    const items= []
-    services.forEach((item, index) => {
-        items.push(
-            item.nombre,
-        )
-    })
-    response ? res.status(200).json(items) : res.status(404).json({message: "No es un id Valido"});
-}
+  const { user_id } = req.user;
+  const response = await User.findById(user_id);
+  const services = response.servicios;
+  const items = [];
+  services.forEach((item, index) => {
+    items.push(item.nombre);
+  });
+  response
+    ? res.status(200).json(items)
+    : res.status(404).json({ message: "No es un id Valido" });
+};
 
 const deleteService = async (req, res) => {
-    const {user_id} = req.user;
-    const id = req.params.id
-    const response = await User.findById(user_id)
-    const services = response.servicios
-    const newServices = services.filter((service) => {
-        return service._id != id
-    })
-    response.servicios = newServices
-    const result = await response.save()
-    result ? res.status(200).json(result) : res.status(404).json({message: "No es un id Valido"});
-}
-
+  const { user_id } = req.user;
+  const id = req.params.id;
+  const response = await User.findById(user_id);
+  const services = response.servicios;
+  const newServices = services.filter((service) => {
+    return service._id != id;
+  });
+  response.servicios = newServices;
+  const result = await response.save();
+  result
+    ? res.status(200).json(result)
+    : res.status(404).json({ message: "No es un id Valido" });
+};
 
 const toggleService = async (req, res) => {
-    const {user_id} = req.user;
-    const id = req.params.id
+  const { user_id } = req.user;
+  const id = req.params.id;
 
-    const resp = await User.findById(user_id)
-    const services = resp.servicios
-    const res2 = services.filter((service) => {
-        return service._id == id
-    })
-    const res3 = res2[0]
-    res3.habilitado= !res3.habilitado
-    const result = await resp.save()
-    result ? res.status(200).json(result) : res.status(404).json({message: "No es un id Valido"});
+  const resp = await User.findById(user_id);
+  const services = resp.servicios;
+  const res2 = services.filter((service) => {
+    return service._id == id;
+  });
+  const res3 = res2[0];
+  res3.habilitado = !res3.habilitado;
+  const result = await resp.save();
+  result
+    ? res.status(200).json(result)
+    : res.status(404).json({ message: "No es un id Valido" });
+};
 
-}
+const updateService = async (req, res) => {
+  const { user_id } = req.user;
+  const id = req.params.id;
+  const form = req.body;
 
+  const usuario = await User.findById(user_id);
+  const service = usuario.servicios.find((s) => s._id == id);
+  service.nombre = form.nombre || service.nombre;
+  service.precio = form.precio || service.precio;
+  service.cantidadVendidos = form.cantidadVendidos || service.cantidadVendidos;
+  service.cantidadDisponibles =
+    form.cantidadDisponibles || service.cantidadDisponibles;
+  service.descripcion = form.descripcion || service.descripcion;
+  service.habilitado = form.habilitado || service.habilitado;
+  service.clientes = form.clientes || service.clientes;
 
+  const result = await usuario.save();
+
+  result
+    ? res.status(200).json(result)
+    : res.status(404).json({ message: "No es un id Valido" });
+};
 
 module.exports = {
-    getServices,
-     createService,
-     itemService,
-        deleteService,
-        toggleService
-}
+  getServices,
+  createService,
+  itemService,
+  deleteService,
+  toggleService,
+  updateService,
+};
--- routes/services.router.js
+++ routes/services.router.js
@@ -3,7 +3,7 @@ import {itemService, getServices, createService, deleteService} from "../control
 import { asureAuth } from "../middlewares/authenticated"; */
 
 const { Router } = require('express');
-const {itemService, getServices, createService, deleteService, toggleService} = require('../controllers/services.controller');
+const {itemService, getServices, createService, deleteService, toggleService, updateService} = require('../controllers/services.controller');
 const { asureAuth } = require('../middlewares/authenticated');
 
 
@@ -14,6 +14,7 @@ router.get("/", [asureAuth], getServices);
 router.get("/items", asureAuth, itemService);
 
 router.patch("/toggle/:id", asureAuth, toggleService);
+router.patch("/update/:id", asureAuth, updateService);
 
 router.post("/create", asureAuth, createService);
 
